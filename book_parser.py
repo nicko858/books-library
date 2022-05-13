eedadd3df327ab9626f056bf7842fd82105d4360
nicko <ikar2021@hotmail.com>
2022-05-13 19:07:34 +0300

    Check for redirects

--- book_parser.py
+++ book_parser.py
@@ -1,24 +1,38 @@
-import requests
 from os import makedirs, path
 
+import requests
 
-def make_book_dir():
-    base_dir = path.dirname(path.realpath(__file__))
-    books_dir = path.join(base_dir, 'books')
+
+def make_book_dir(books_dir):
     try:
         makedirs(books_dir, exist_ok=True)
-        return(books_dir)
     except OSError as error:
         print(error)
+        return
+    return books_dir
+
+
+def check_for_redirect(respose):
+    if response.history:
+        raise requests.HTTPError
 
 
 if __name__ == '__main__':
-    url = 'https://tululu.org/txt.php?id={0}'
+    base_url = 'https://tululu.org/txt.php?id={0}'
+    base_dir = path.dirname(path.realpath(__file__))
+    books_dir = path.join(base_dir, 'books')
     books_count = 10
-    books_dir = make_book_dir()
-    for book_id in range(1, books_count+1):
-        response = requests.get(url.format(book_id))
-        response.raise_for_status
+    book_start_id = 1
+    books_dir = make_book_dir(books_dir)
+    if not books_dir:
+        exit('Не удалось создать директорию для книг!')
+    for book_id in range(book_start_id, books_count+1):
+        response = requests.get(base_url.format(book_id))
         book_text = response.text
-        with open(path.join(books_dir, 'id{0}.txt'.format(book_id)), 'w') as file_handler:
+        book_path = path.join(books_dir, 'id{0}.txt'.format(book_id))
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            continue
+        with open(book_path, 'w') as file_handler:
             file_handler.write(book_text)
