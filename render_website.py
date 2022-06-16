7bfc15984599dafced5353af78cb300236f6da47
nicko <ikar2021@hotmail.com>
2022-06-16 23:26:26 +0300

    Fix paginator

--- render_website.py
+++ render_website.py
@@ -19,14 +19,14 @@ def on_reload():
     pages_path = path.join(BASE_DIR, 'pages')
     makedirs(pages_path, exist_ok=True)
     for idx, books in enumerate(chuncked_books):
+        is_first_page = False
+        is_last_page = False
         if idx == 0:
-            previous_page = len(chuncked_books) - 1
-        else:
-            previous_page = idx - 1
+            is_first_page = True
         if idx == last_chuncked_element:
-            next_page = 0
-        else:
-            next_page = idx + 1
+            is_last_page = True
+        next_page = idx + 1
+        previous_page = idx - 1
         page_path = path.join(pages_path, 'index{0}.html'.format(idx))
         template = env.get_template('template.html')
         chuncked_books = list(chunked(books, books_per_column))
@@ -37,6 +37,8 @@ def on_reload():
             pages_path=pages_path,
             next_page=next_page,
             previous_page=previous_page,
+            is_first_page=is_first_page,
+            is_last_page=is_last_page,
         )
         with open(page_path, 'w', encoding='utf8') as html:
             html.write(rendered_page)
